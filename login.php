b0e49ffbccbbb7f43c7d9a726c079b463e13f978
Rychard Guedes <guedes.rychard@gmail.com>
2016-09-01 22:52:17 -0300

    login problem solving

--- login.php
+++ login.php
@@ -1,42 +1,44 @@
 <?php
 
 session_start();
-$login = $_POST['email'] ?? null;
-$password = $_POST['password'] ?? null;
-
+$login = $_GET['login'] ?? null;
+$password = $_GET['password'] ?? null;
+var_dump($password);
 // connection to db
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "abc123";
+$dbname = "BANCOSUPERVISORIO";
+$portnumber = "3306";
 
 //Conexão mysql
-$conexao = mysql_connect($servername, $username, $password);
+$conexao = new mysqli($servername, $username, $password, $dbname, $portnumber) or die("Erro de conexão.");
 
 // Check connection
 if ($conexao->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//echo "Connected successfully";
 
-//Seleciona o banco de dados
-$selecionabd = mysql_select_db($database,$conexao)
-            or die ("Banco de dados inexistente.");
+// //Seleciona o banco de dados
+// $selecionabd = mysql_select_db($database,$conexao)
+//             or die ("Banco de dados inexistente.");
 
 // Verifica no cadastro de médicos
-$sql = "SELECT *
-FROM Medico
-WHERE email = '$login'
-AND senha = '$password'";
+$sql = "SELECT * FROM Paciente WHERE email = '$login'";
 
-$resultado = mysql_query($sql,$conexao) or die ("Erro na seleção da tabela.");
+$resultado = mysqli_query($conexao, $sql) or die ("Erro na seleção da tabela 1.");
+$row = mysqli_fetch_array($resultado);
+var_dump($row);
 
-if (mysql_num_rows ($resultado) == 0) {
+if (mysqli_num_rows($resultado) == 0) {
   // Verifica no cadastro de pacientes
   $sql = "SELECT *
-  FROM Paciente
-  WHERE email = '$login'
-  AND senha = '$password'";
-  $resultado = mysql_query($sql,$conexao) or die ("Erro na seleção da tabela.");
+  FROM Medico
+  WHERE email = 'guedes.rychard@gmail.com'
+  AND senha = '123'";
+  $resultado = mysqli_query($conexao, $sql) or die ("Erro na seleção da tabela 2.");
+  var_dump('mizera');
 }
 
 //Caso consiga logar cria a sessão
@@ -46,10 +48,15 @@ if (mysql_num_rows ($resultado) > 0) {
 
     $_SESSION['login'] = $login;
     $_SESSION['password'] = $password;
+
+
+    header('Location: http://localhost:8080/aula/supervisorio-biomedico/main.htmlwww.example.com/');
 }
 
 //Caso contrário redireciona para a página de autenticação
 else {
+
+    var_dump('login');
     //Destrói
     session_destroy();
 
@@ -58,7 +65,8 @@ else {
     unset ($_SESSION['password']);
 
     //Redireciona para a página de autenticação
-    header('location:login.php');
+    header('location:login.html');
+
 
 }
 
